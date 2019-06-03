2305abd91e4241d76382281f85ee7c0d1a74ff50
Victor Guillet <victor.guillet@gmail.com>
2019-06-03 17:35:36 +0200

    updated order_gen and account_gen

--- PhyTrade/Trade_simulations/Tools/ACCOUNT_gen.py
+++ PhyTrade/Trade_simulations/Tools/ACCOUNT_gen.py
@@ -1,10 +1,11 @@
 """
 Contains information about a Tradebot's account, along with it's transaction history etc...
 """
+from PhyTrade.Trade_simulations.Tools.ORDER_gen import ORDER_gen
 
 
 class ACCOUNT:
-    def __init__(self, initial_funds=1000, initial_assets=[]):
+    def __init__(self, initial_funds=1000, initial_orders=[]):
         """
         Stores the states of a trading account. Stores account history in:
             -> funds_history
@@ -14,75 +15,212 @@ class ACCOUNT:
             -> net_worth_history
 
         Contains the convert funds-to-assets and convert assets-to-funds methods to perform transactions
+        The update_account must be run for each new day to allow for a correct updating of the orders
 
         :param initial_funds: Initial funds in the account
-        :param initial_assets: Initial assets in the account (in number of stocks)
+        :param initial_orders: Initial orders in the account (as order class instances)
         """
 
+        # ---- Account initialisation
         self.initial_funds = initial_funds
-        self.initial_assets = initial_assets
+        self.initial_orders = initial_orders
 
-        self.current_funds = initial_funds
-        self.current_assets = initial_assets
-        
-        self.funds_history = []
-        self.assets_history = []
+        # ---- Records initialisation
+        # --> Record initial orders in content
+        self.content = {}
+        for order in self.initial_orders:
+            self.create_content_entry(order.ticker)
+            self.add_order_to_content(order.ticker, order)
+
+        # --> Initialise history lsts
+        self.orders_history = []
+
         self.net_worth_history = []
+        self.funds_history = []
+        self.net_profit_percentage_history = []
+        self.day_profit_percentage_history = []
 
+        # ---- Current param setup
+        self.current_funds = initial_funds
+        self.current_date = None
+
+        # ---- Simple investment initialisation
         self.simple_investment_assets = None
         self.simple_investment_net_worth = []
 
-    def convert_funds_to_assets(self, current_value, investment_per_trade):
-        """
-        Used to perform buy operations
+    def create_content_entry(self, ticker, current_value=None):
+        self.content[ticker] = {}
+        self.content[ticker]["Current_price"] = current_value
+        self.content[ticker]["Open_order_count"] = 0
+        self.content[ticker]["Open_orders"] = []
+        self.content[ticker]["Close_order_count"] = 0
+        self.content[ticker]["Close_orders"] = []
+        self.content[ticker]["Net_worth"] = []
 
-        :param current_value: Current value of the stock brought
+    def add_order_to_content(self, ticker, order):
+        self.content[ticker]["Open_orders"].append(order)
+        self.content[ticker]["Open_order_count"] += 1
+
+    def update_account(self, current_date, current_values):
+        """
+        Used to update daily values of the account, should always be ran first if new day!
+        New tickers can be included here.
+
+        :param current_date: Current traded date as string
+        :param current_values: Dictionary of current values with tickers as key
+        """
+        self.current_date = current_date
+
+        for ticker in self.content.keys():
+            assert ticker in current_values.keys()
+
+        # --> Update current prices and add new tickers
+        for ticker in current_values.keys():
+            if ticker not in self.content.keys():
+                self.create_content_entry(ticker, current_values[ticker])
+            else:
+                self.content[ticker]["Current_price"] = current_values[ticker]
+
+        # --> Update orders
+        for ticker in current_values.keys():
+            if len(self.content[ticker]["Open_orders"]) != 0:
+                for order in self.content[ticker]["Open_orders"]:
+                    order.update_order(current_values[ticker])
+
+    def convert_funds_to_assets(self, ticker, investment_per_trade):
+        """
+        Used to perform a buy operation for a specific ticker at a given date
+
+        :param ticker: Ticker traded
         :param investment_per_trade: Amount of investment to be performed
         """
 
+        self.add_order_to_content(ticker, ORDER_gen(ticker,
+                                                    self.current_date,
+                                                    investment_per_trade/self.content[ticker]["Current_price"],
+                                                    self.content[ticker]["Current_price"]))
+
+        # --> Update funds
         self.current_funds = self.current_funds - investment_per_trade
-        self.current_assets = self.current_assets + investment_per_trade / current_value
 
-        self.funds_history.append(self.current_funds)
-        self.assets_history.append(self.current_assets)
-        self.net_worth_history.append(self.current_funds + self.current_assets * current_value)
-
-    def convert_assets_to_funds(self, current_value, assets_sold_per_trade):
+    def convert_assets_to_funds(self, ticker, assets_sold_per_trade):
         """
-        Used to perform sell operation
+        Used to perform a sell operation for a specific ticker at a given date
 
-        :param current_value: Current value of the stock sold
+        :param ticker: Ticker traded
         :param assets_sold_per_trade: Amount of assets to be sold
         """
-        self.current_funds = self.current_funds + assets_sold_per_trade * current_value
-        self.current_assets = self.current_assets - assets_sold_per_trade
+        # TODO: Develop order sell choice
 
-        self.funds_history.append(self.current_funds)
-        self.assets_history.append(self.current_assets)
-        self.net_worth_history.append(self.current_funds + self.current_assets * current_value)
+        sell_worth = 0
+        while sell_worth < assets_sold_per_trade:
+            # --> Select which order to close
+            if len(self.content[ticker]["Open_orders"]) > 1:
+                diff = []
 
-    def record_net_worth(self, current_value):
-        self.funds_history.append(self.current_funds)
-        self.assets_history.append(self.current_assets)
-        self.net_worth_history.append(self.current_funds + self.current_assets * current_value)
+                # --> Calc orders value-difference
+                for order in self.content[ticker]["Open_orders"]:
+                    diff.append(abs(assets_sold_per_trade - order.current_worth))
 
-    def calc_net_worth(self, current_value):
-        return self.current_funds+self.current_assets*current_value
+                # --> Rearrange orders according to value difference (min to max)
+                for i in range(1, len(diff)):
+                    if diff[i] < diff[i - 1]:
+                        diff[i], diff[i - 1] = diff[i - 1], diff[i]
+                        self.content[ticker]["Open_orders"][i], self.content[ticker]["Open_orders"][i - 1] = \
+                            self.content[ticker]["Open_orders"][i - 1], self.content[ticker]["Open_orders"][i]
 
-    def calc_net_profit(self, current_value):
-        return self.current_funds+self.current_assets*current_value-self.initial_funds
+            # --> Close order
+            order = self.content[ticker]["Open_orders"][0]
+            order.close_order()
 
-    def start_simple_investment(self, current_value, initial_investment=1000):
-        self.simple_investment_assets = initial_investment/current_value
-        print(self.simple_investment_assets)
+            # --> Record close worth
+            sell_worth += order.close_worth
 
-    def calc_simple_investment_value(self, current_value):
-        self.simple_investment_net_worth.append(self.simple_investment_assets*current_value)
+            # --> Move closed order to order history
+            self.orders_history.append(self.content[ticker]["Open_orders"].pop(order))
+            self.content[ticker]["Open_order_count"] -= 1
 
-    def print_account_status(self, current_value):
+            if self.content[ticker]["Open_order_count"] == 0:
+                break
+
+        # --> Update funds
+        self.current_funds = self.current_funds + sell_worth
+
+    # ----------------------------------- Net functions
+    def calc_net_worth(self):
+        """
+        Used to compute current net worth
+
+        :return: Net worth
+        """
+        net_worth = self.net_worth_history[-1]
+        for order in self.open_orders:
+            order.update_order(self.current_value)
+            net_worth += order.current_order_worth
+
+        return net_worth
+
+    def record_net_worth(self):
+        """
+        Used to record current net worth to net_worth_history
+        """
+        self.net_worth_history.append(self.calc_net_worth())
+
+    def calc_day_profit_percentage(self):
+        """
+        Used to compute current day profit percentage
+
+        :return: Day profit percentage
+        """
+        return (self.calc_net_worth()-self.net_worth_history[-1])/self.net_worth_history[-1]
+
+    def record_day_profit_percentage(self):
+        """
+        Used to record current day profit percentage
+        """
+        self.day_profit_percentage_history.append(self.calc_day_profit_percentage())
+
+    # ----------------------------------- Ticker specific functions
+    def calc_ticker_net_worth(self, ticker):
+        """
+        Used to compute net worth of a specific ticker
+
+        :param ticker: Ticker to be evaluated
+        :return: Ticker specific current worth
+        """
+        net_worth = 0
+        for order in self.open_orders:
+            if order.ticker == ticker:
+                net_worth += order.current_worth
+
+        return net_worth
+
+    def calc_ticker_current_profit(self, ticker):
+        """
+        Used to compute profit of a specific ticker
+
+        :param ticker: Ticker to be evaluated
+        :return: Ticker specific current profit
+        """
+        profit = 0
+        for order in self.open_orders:
+            if order.ticker == ticker:
+                profit += order.current_return
+
+        return profit
+
+    # ----------------------------------- Simple investment functions
+    def start_simple_investment(self, tickers=[], initial_investments=[1000]):
+        self.simple_investment = ORDER_gen(ticker, self.current_date, initial_investment/self.current_value, self.current_value)
+
+    def calc_simple_investment_value(self):
+        self.simple_investment_net_worth.append(self.simple_investment_assets*self.current_value)
+
+    # ----------------------------------- Print/plot functions
+    def print_account_status(self):
         print("Money =", self.current_funds, "$")
-        print("Share owned=", self.current_assets)
-        print("Total asset value=", self.calc_net_worth(current_value), "$")
+        print("Number of open orders =", len(self.open_orders))
+        print("Total asset value=", self.calc_net_worth(), "$")
 
     def plot_net_worth(self, dates):
         import matplotlib.pyplot as plt
--- PhyTrade/Trade_simulations/Tools/ORDER_gen.py
+++ PhyTrade/Trade_simulations/Tools/ORDER_gen.py
@@ -1,7 +1,7 @@
 
 
 class ORDER_gen:
-    def __init__(self, open_date, asset_count, open_price):
+    def __init__(self, ticker, open_date, asset_count, open_price):
         """
         Used to generate orders
 
@@ -10,15 +10,18 @@ class ORDER_gen:
         :param open_price: Price at order creation
         """
         # ---- Define open order parameters
+        self.ticker = ticker
         self.open_date = open_date
         self.open_price = open_price
 
         self.asset_count = asset_count
-        self.open_order_worth = open_price*asset_count
+        self.open_worth = open_price*asset_count
 
         # ---- Define current order parameters
-        self.current_order_worth = self.open_order_worth
-        self.return_pct = 0
+        self.current_date = open_date
+        self.current_price = open_price
+        self.current_worth = self.open_worth
+        self.return_percent = 0
         self.current_return = 0
 
         # ---- Define close order parameters
@@ -26,37 +29,37 @@ class ORDER_gen:
         self.close_date = None
         self.close_price = None
 
-        self.close_order_worth = None
+        self.close_worth = None
         self.close_return = None
         self.return_outcome = None
 
-    def update_order(self, current_price):
+    def update_order(self, current_date, current_price):
         """
         Used to update order parameters
 
+        :param current_date: Date of order update
         :param current_price: Price at order update
         """
-        self.current_order_worth = current_price * self.asset_count
-        self.return_pct = ((current_price - self.open_price) / self.open_price) * 100
+        self.current_date = current_date
+        self.current_price = current_price
+        self.current_worth = current_price * self.asset_count
+        self.return_percent = ((current_price - self.open_price) / self.open_price) * 100
         self.current_return = self.asset_count * (current_price - self.open_price)
 
-    def close_order(self, close_date, close_price):
+    def close_order(self):
         """
         Use to close order and update order parameters
 
         return_outcome is used to record whether order was profitable or not
-
-        :param close_date: Date of order close
-        :param close_price: Price at order close
         """
         self.closed = True
-        self.close_date = close_date
-        self.close_price = close_price
+        self.close_date = self.current_date
+        self.close_price = self.current_price
 
-        self.close_order_worth = close_price * self.asset_count
-        self.close_return = self.asset_count * (close_price - self.open_price)
+        self.close_worth = self.current_worth
+        self.close_return = self.current_return
 
-        if self.close_order_worth >= self.open_order_worth:
+        if self.close_worth >= self.open_worth:
             self.return_outcome = 1
         else:
             self.return_outcome = 0
